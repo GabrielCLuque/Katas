c202e7fe4741479cc6dbf1aa123ee80c0a983a60
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-10-02 12:16:04 +0200

    Update kata-word-frame.php

--- kata-word-frame.php
+++ kata-word-frame.php
@@ -1,41 +1,42 @@
 <?php
 $frase = "has debugao ya";
 $porciones = explode(" ", $frase);
-$alm=array();
-$tamano = 0;
 
-foreach($porciones as $porcion){
-if ($tamano <= strlen($porcion)){
-    $tamano =  strlen($porcion);
-}
-}
-
- 
- for ($i=0; $i<$tamano+4; $i++){
-    echo  "#";
- }
-
-
- echo "</br>";
-
- 
- $espacios=0;
-
-foreach($porciones as $porcion){
-echo  "# " . $porcion;
-$espacios = strlen($porcion) - $tamano;
-if(strlen($porcion) <= $tamano){
-    for ($i = 0; $i <= $espacios; $i++){
-        echo " ";
+function gettamano($porciones):int {
+    $tamano=0;
+foreach ($porciones as $porcion) {
+    if ($tamano < strlen($porcion)) {
+        $tamano = strlen($porcion);
     }
-    echo " #". "</br>";
+}
+return $tamano ;
 }
 
+$tamano = gettamano($porciones);
+
+for ($i = 0; $i < $tamano + 3; $i++) {
+    echo "#";
+}
+echo "<br>";
+
+
+
+foreach ($porciones as $porcion) {
+    echo "# " . $porcion;
+if(strlen($porcion)!=$tamano){
+    $espacios = $tamano - strlen($porcion);
+    for ($x = 0; $x < $espacios; $x++) {
+        echo "..";
+    }
+}
+    echo " #"."<br>";
 }
 
-for ($i=0; $i<$tamano+4; $i++){
-    echo  "#";
- }
- 
+
+for ($i = 0; $i < $tamano + 3; $i++) {
+    echo "#";
+}
+echo "<br>";
+
 
 ?>
